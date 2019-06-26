55180be28956c027f7c9663650d7680e6982d245
Kayla Silvey <ksilvey19@gmail.com>
2019-06-25 20:07:56 -0500

    create display route for all products and individual products

--- app/controllers/api/products_controller.rb
+++ app/controllers/api/products_controller.rb
@@ -3,4 +3,39 @@ class Api::ProductsController < ApplicationController
     @product = Product.first
     render "uppers_and_downers.json.jb"
   end
+
+  def whiskey_method
+    @product = Product.second
+    render "uppers_and_downers.json.jb"
+  end
+
+  def heart_butt_method
+    @product = Product.third
+    render "uppers_and_downers.json.jb"
+  end
+
+  def trash_queen_method
+    @product = Product.fourth
+    render "uppers_and_downers.json.jb"
+  end
+
+  def home_sweet_method
+    @product = Product.fifth
+    render "uppers_and_downers.json.jb"
+  end
+
+  def crystal_heart_method
+    @product = Product
+    render "uppers_and_downers.json.jb"
+  end
+
+  def death_of_magic_method
+    @product = Product.last
+    render "uppers_and_downers.json.jb"
+  end
+
+  def all_products_method
+    @products = Product.all
+    render "all_products.json.jb"
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,12 @@ Rails.application.routes.draw do
   # EXAMPLE JSON ROUTE WITH API NAMESPACE
   namespace :api do
     get "/uppers_and_downers" => "products#uppers_and_downers_method"
+    get "/friskeywhiskey" => "products#whiskey_method"
+    get "/heart_butt" => "products#heart_butt_method"
+    get "/trash_queen" => "products#trash_queen_method"
+    get "/home_sweet" => "products#home_sweet_method"
+    get "/crystal_heart" => "products#crystal_heart_method"
+    get "/death_of_magic" => "products#death_of_magic_method"
+    get "/all_products" => "products#all_products_method"
   end
 end

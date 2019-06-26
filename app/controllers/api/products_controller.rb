1f5adec940dbdaf5bc112a847b41f62f81207ce1
Kayla Silvey <ksilvey19@gmail.com>
2019-06-25 19:30:31 -0500

    create route to display JSON for first product

--- app/controllers/api/products_controller.rb
+++ app/controllers/api/products_controller.rb
@@ -1,2 +1,6 @@
 class Api::ProductsController < ApplicationController
+  def uppers_and_downers_method
+    @product = Product.first
+    render "uppers_and_downers.json.jb"
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   # get "/photos" => "photos#index"
 
   # EXAMPLE JSON ROUTE WITH API NAMESPACE
-  # namespace :api do
-  #   get "/photos" => "photos#index"
-  # end
+  namespace :api do
+    get "/uppers_and_downers" => "products#uppers_and_downers_method"
+  end
 end

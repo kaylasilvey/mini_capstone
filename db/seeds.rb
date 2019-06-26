0636289732e3c9f5380c519c03477b60ecdf53f5
Kayla Silvey <ksilvey19@gmail.com>
2019-06-25 19:19:55 -0500

    seed database

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,52 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+product = Product.create({
+  name: "Uppers and Downers Enamel Pin Set",
+  price: 12.95,
+  image_url: "http://annalisaillustration.com/wp-content/uploads/2018/08/IMG_2637-1030x1030.jpg",
+  description: "Keep things on the level",
+})
+
+product = Product.create({
+  name: "Whiskey Makes Me Friskey Pin",
+  price: 2.95,
+  image_url: "http://annalisaillustration.com/wp-content/uploads/2012/03/IMG_4708-773x1030.jpg",
+  description: "Cheers!",
+})
+
+product = Product.create({
+  name: "Heart Butt Enamel Pin",
+  price: 8.00,
+  image_url: "http://annalisaillustration.com/wp-content/uploads/2012/03/product_image.jpg",
+  description: "Let it all hang out",
+})
+
+product = Product.create({
+  name: "Trash Queen Print",
+  price: 15.00,
+  image_url: "http://annalisaillustration.com/wp-content/uploads/2012/03/Trashqueen_small.jpg",
+  description: "You might be trash, but you're royal trash.",
+})
+
+product = Product.create({
+  name: "Home Sweet Home Print",
+  price: 15.00,
+  image_url: "http://annalisaillustration.com/wp-content/uploads/2012/03/UmWwBQ-66dAKbB5KFdjXRCqt1yVWl06aijVoRD2emx4FERLOZoN8d-zQr439SpnG-796x1030.jpg",
+  description: "Cuz there's no place like it.",
+})
+
+product = Product.create({
+  name: "Crystal Heart Print",
+  price: 15.00,
+  image_url: "http://annalisaillustration.com/wp-content/uploads/2015/10/Inktober_2015_day30-773x1030.jpg",
+  description: "Don't go breaking my...",
+})
+
+product = Product.create({
+  name: "Death of Magic Print",
+  price: 15.00,
+  image_url: "http://annalisaillustration.com/wp-content/uploads/2015/10/Inktober_2015_day26-773x1030.jpg",
+  description: "Magic is dead. Bummer.",
+})

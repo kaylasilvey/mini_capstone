45790ec48d6db370c440210bcfeff1ba2c574f71
Kayla Silvey <ksilvey19@gmail.com>
2019-06-26 19:05:45 -0500

    change url to use params to access product by id

--- app/controllers/api/products_controller.rb
+++ app/controllers/api/products_controller.rb
@@ -1,36 +1,12 @@
 class Api::ProductsController < ApplicationController
-  def uppers_and_downers_method
-    @product = Product.first
+  def product_url_method
+    @product = Product.find_by(id: params["product_url"].to_i)
     render "uppers_and_downers.json.jb"
   end
 
-  def whiskey_method
-    @product = Product.second
-    render "uppers_and_downers.json.jb"
-  end
-
-  def heart_butt_method
-    @product = Product.third
-    render "uppers_and_downers.json.jb"
-  end
-
-  def trash_queen_method
-    @product = Product.fourth
-    render "uppers_and_downers.json.jb"
-  end
-
-  def home_sweet_method
-    @product = Product.fifth
-    render "uppers_and_downers.json.jb"
-  end
-
-  def crystal_heart_method
-    @product = Product
-    render "uppers_and_downers.json.jb"
-  end
-
-  def death_of_magic_method
-    @product = Product.last
+  def product_query_method
+    product = params["id"].to_i
+    @product = Product.find_by(id: product)
     render "uppers_and_downers.json.jb"
   end
 
@@ -38,4 +14,39 @@ class Api::ProductsController < ApplicationController
     @products = Product.all
     render "all_products.json.jb"
   end
+
+  # def uppers_and_downers_method
+  #   @product = Product.first
+  #   render "uppers_and_downers.json.jb"
+  # end
+
+  # def whiskey_method
+  #   @product = Product.second
+  #   render "uppers_and_downers.json.jb"
+  # end
+
+  # def heart_butt_method
+  #   @product = Product.third
+  #   render "uppers_and_downers.json.jb"
+  # end
+
+  # def trash_queen_method
+  #   @product = Product.fourth
+  #   render "uppers_and_downers.json.jb"
+  # end
+
+  # def home_sweet_method
+  #   @product = Product.fifth
+  #   render "uppers_and_downers.json.jb"
+  # end
+
+  # def crystal_heart_method
+  #   @product = Product
+  #   render "uppers_and_downers.json.jb"
+  # end
+
+  # def death_of_magic_method
+  #   @product = Product.last
+  #   render "uppers_and_downers.json.jb"
+  # end
 end

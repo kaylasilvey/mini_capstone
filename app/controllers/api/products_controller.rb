8d297fb766114874f6ddccb545bd58471e654324
Kayla Silvey <ksilvey19@gmail.com>
2019-07-01 19:16:41 -0500

    add a happy/sad path render in update controller

--- app/controllers/api/products_controller.rb
+++ app/controllers/api/products_controller.rb
@@ -30,8 +30,11 @@ class Api::ProductsController < ApplicationController
     @product.price = params["price"] || @product.price
     @product.image_url = params["image_url"] || @product.image_url
     @product.description = params["description"] || @product.description
-    @product.save
-    render "show.json.jb"
+    if @product.save
+      render "show.json.jb"
+    else
+      render json: { errors: @product.errors.full_messages }, status: :unprocessable_entity
+    end
   end
 
   def destroy

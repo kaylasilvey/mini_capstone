4bef757391b13b1469e279e0a30fa9486cd9b41f
Kayla Silvey <ksilvey19@gmail.com>
2019-07-16 19:20:28 -0500

    add RESTful HTML edit/update/destroy actions

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -25,4 +25,26 @@ class ProductsController < ApplicationController
     @product.save
     redirect_to "/products/#{@product.id}"
   end
+
+  def edit
+    @product = Product.find_by(id: params[:id])
+    @supplier = Supplier.all
+    render "edit.html.erb"
+  end
+
+  def update
+    @product = Product.find_by(id: params[:id])
+    @product.name = params[:name]
+    @product.price = params[:price]
+    @product.description = params[:description]
+    @product.supplier_id = params[:supplier_id]
+    @product.save
+    redirect_to "/products/#{@product.id}"
+  end
+
+  def destroy
+    @product = Product.find_by(id: params[:id])
+    @product.destroy
+    redirect_to "/products"
+  end
 end
